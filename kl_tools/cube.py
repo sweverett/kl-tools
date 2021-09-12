903a62b118d5da963c9388958f802cfcbc9e936c
Spencer Everett <spencerweverett@gmail.com>
2021-09-12 12:11:26 -0700

    Can now instantiate a `DataCube` from a fitscube file using `FitsDatacube`.

--- kl_tools/cube.py
+++ kl_tools/cube.py
@@ -1,4 +1,9 @@
 import numpy as np
+from astropy.io import fits
+import galsim
+import os
+import pickle
+import utils
 import pudb
 
 class DataCube(object):
@@ -8,7 +13,7 @@ class DataCube(object):
 #     at various wavelength slices
 #     '''
 
-    def __init__(self, data=None, shape=None, lambda_array=None):
+    def __init__(self, data=None, shape=None, bandpasses=None):
         '''
         Initialize either a filled DataCube from an existing numpy
         array or an empty one from a given shape
@@ -18,8 +23,8 @@ class DataCube(object):
         shape: A 3-tuple in the format of (Nx, Ny, Nspec)
                where (Nx, Ny) are the shapes of the image slices
                and Nspec is the Number of spectral slices.
-        lambda_data: A list or numpy array of lambda of each data
-               slice. Assumed to be the center of the spectral window.
+        bandpasses: A list of galsim.Bandpass objects containing
+               throughput function, lambda window, etc.
         '''
 
         if data is None:
@@ -38,18 +43,32 @@ class DataCube(object):
         else:
             assert len(data.shape) == 3
 
+            if bandpasses is None:
+                raise ValueError('Must pass bandpasses if data is not None!')
 
             self._data = data
             self.shape = data.shape
 
-            if data.shape[2] != len(lambda_array):
-                raise ValueError('The length of the lambda_array must ' + \
+            if data.shape[2] != len(bandpasses):
+                raise ValueError('The length of the bandpasses must ' + \
                                  'equal the length of the third data dimension!')
             self.Nx = self.shape[0]
             self.Ny = self.shape[1]
             self.Nspec = self.shape[2]
 
-        self.lambda_array = lambda_array
+        self.bandpasses = bandpasses
+
+        # Not necessarily needed, but could help ease of access
+        self.lambda_unit = self.bandpasses[0].wave_type
+        self.lambdas = [] # Tuples of bandpass bounds in unit of bandpass
+        for bp in bandpasses:
+            li = bp.blue_limit
+            le = bp.red_limit
+            self.lambdas.append((li, le))
+
+            # Make sure units are consistent
+            # (could generalize, but not necessary)
+            assert bp.wave_type == self.lambda_unit
 
         self.slices = self._construct_slice_list()
 
@@ -70,8 +89,58 @@ class DataCube(object):
         self.slices = SliceList()
 
         for i in range(self.Nspec):
-            lam = self.lambda_array[i]
-            self.slices.append(Slice(self._data[:,:,i], lam))
+            bp = self.bandpasses[i]
+            self.slices.append(Slice(self._data[:,:,i], bp))
+
+        return
+
+class FitsDataCube(DataCube):
+    '''
+    Same as Datacube, but instantiated from a fitscube file
+    and associated file containing bandpass list
+
+    We assume the same structure as galsim.fits.writeCube()
+
+    cubefile: location of fits cube
+    bandpasses: either a filename of a bandpass list or the list
+    '''
+
+    def __init__(self, cubefile, bandpasses, dir=None):
+        if dir is not None:
+            cubefile = os.path.join(dir, cubefile)
+
+        self.cubefile = cubefile
+
+        # fits_cube = fits.open(cubefile)
+        # pudb.set_trace()
+        # Nimages = len(fits_cube)
+        # im_shape = fits_cube[0].data.shape
+        # data = np.zeros((im_shape[0], im_shape[1], Nimages))
+
+        # for i, im in enumerate(fits_cube):
+        #     data[:,:,i] = im
+        fits_cube = galsim.fits.readCube(cubefile)
+        Nimages = len(fits_cube)
+        im_shape = fits_cube[0].array.shape
+        data = np.zeros((im_shape[0], im_shape[1], Nimages))
+
+        # pudb.set_trace()
+        for i, im in enumerate(fits_cube):
+            data[:,:,i] = im.array
+
+        if isinstance(bandpasses, str):
+            bandpass_file = bandpasses
+            if '.pkl' in bandpass_file:
+                with open(bandpass_file, 'rb') as f:
+                    bandpasses = pickle.load(f)
+            else:
+                raise Exception('For now, only pickled lists of ' +\
+                                'galsim.Bandpass objects are accepted')
+        else:
+            if not isinstance(bandpasses, list):
+                raise Exception('For now, must pass bandpasses as either filename or list!')
+
+        super(FitsDataCube, self).__init__(data=data, bandpasses=bandpasses)
 
         return
 
@@ -147,12 +216,17 @@ class SliceList(list):
 class Slice(object):
     '''
     Base class of an abstract DataCube slice,
-    corresponding to a source observation at
-    give wavelength
+    corresponding to a source observation in a given
+    bandpass
     '''
-    def __init__(self, data, wavelength):
+    def __init__(self, data, bandpass):
         self._data = data
-        self.wavelength = wavelength
+        self.bandpass = bandpass
+
+        self.red_limit = bandpass.red_limit
+        self.blue_limit = bandpass.blue_limit
+        self.dlamda = self.red_limit - self.blue_limit
+        self.lambda_unit = bandpass.wave_type
 
         return
 
@@ -160,25 +234,58 @@ class Slice(object):
 def main():
 
     li, le, dl = 500, 600, 1
-    lambda_array = np.arange(li, le+dl, dl)
+    lambdas = np.arange(li, le, dl)
 
-    shape = (100, 100, len(lambda_array))
+    throughput = '0.85'
+    unit = 'nm'
+    zp = 30
+    bandpasses = []
+
+    print('Building bandpasses')
+    for l1, l2 in zip(lambdas, lambdas+1):
+        bandpasses.append(galsim.Bandpass(
+            throughput, unit, blue_limit=l1, red_limit=l2, zeropoint=zp
+            ))
+
+    Nspec = len(bandpasses)
+
+    shape = (100, 100, Nspec)
     data = np.zeros(shape)
 
+    print('Building Slice object')
     n = 50 # slice num
-    wave = 500 # nm
-    s = Slice(data[:,:,n], wave)
+    s = Slice(data[:,:,n], bandpasses[n])
 
     sl = SliceList()
     sl.append(s)
 
-    cube = DataCube(data=data, lambda_array=lambda_array)
+    print('Building DataCube object from array')
+    cube = DataCube(data=data, bandpasses=bandpasses)
+
+    print('Building DataCube from simulated fitscube file')
+    mock_dir = os.path.join(utils.TEST_DIR,
+                            'mocks',
+                            'COSMOS')
+    test_cubefile = os.path.join(mock_dir,
+                                 'kl-mocks-COSMOS-001.fits')
+    bandpass_file = os.path.join(mock_dir,
+                                 'bandpass_list.pkl')
+    if (os.path.exists(test_cubefile)) and (os.path.exists(bandpass_file)):
+        print('Building from pickled bandpass list file')
+        fits_cube = FitsDataCube(test_cubefile, bandpass_file)
+        print('Building from bandpass list directly')
+        with open(bandpass_file, 'rb') as f:
+            bandpasses = pickle.load(f)
+        fits_cube = FitsDataCube(test_cubefile, bandpasses)
+    else:
+        print('Files missing - skipping test')
 
     print('Done!')
 
     return 0
 
 if __name__ == '__main__':
+    print('Starting tests')
     rc = main()
 
     if rc == 0:

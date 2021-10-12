28f8df319539147dc41fd12483fdcf690f050dca
Spencer Everett <spencerweverett@gmail.com>
2021-10-12 13:05:53 -0700

    First pass at `schwimmbad` implementation

--- kl_tools/test_mcmc_run.py
+++ kl_tools/test_mcmc_run.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
 import pickle
+import schwimmbad
+from schwimmbad import MPIPool
 from argparse import ArgumentParser
 from astropy.units import Unit
 import galsim as gs
@@ -24,155 +26,172 @@ parser.add_argument('nsteps', type=int,
 parser.add_argument('--sampler', type=str, choices=['zeus', 'emcee'],
                     default='emcee',
                     help='Which sampler to use for mcmc')
-parser.add_argument('--ncores', type=int, default=1,
-                    help='Number of CPUs to use')
+# parser.add_argument('--ncores', type=int, default=1,
+#                     help='Number of CPUs to use')
+parser.add_argument('--run_name', type=str, default='',
+                    help='Name of mcmc run')
 parser.add_argument('--show', action='store_true', default=False,
                     help='Set to show test plots')
 
-def main(args):
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--ncores', default=1, type=int,
+                    help='Number of processes (uses `multiprocessing`)')
+group.add_argument('--mpi', dest='mpi', default=False, action='store_true',
+                   help='Run with MPI.')
+
+def main(args, pool):
 
     nsteps = args.nsteps
     sampler = args.sampler
     ncores = args.ncores
+    run_name = args.run_name
     show = args.show
 
-    outdir = os.path.join(utils.TEST_DIR, 'test-mcmc-run')
-    utils.make_dir(outdir)
+    # TODO: might only work for MPI
+    if not pool.is_master():
+        pool.wait()
+        sys.exit(0)
 
-    true_pars = {
-        'g1': 0.05,
-        'g2': -0.025,
-        'theta_int': np.pi / 3,
-        'sini': 0.8,
-        'v0': 10.,
-        'vcirc': 200,
-        'rscale': 5,
-    }
-
-    # additional args needed for prior / likelihood evaluation
-    halpha = 656.28 # nm
-    R = 5000.
-    z = 0.3
-    pars = {
-        'Nx': 30, # pixels
-        'Ny': 30, # pixels
-        'true_flux': 1e5, # counts
-        'true_hlr': 5, # pixels
-        'v_unit': Unit('km / s'),
-        'r_unit': Unit('kpc'),
-        'z': z,
-        'spec_resolution': R,
-        # 'line_std': 0.17,
-        'line_std': halpha * (1.+z) / R, # emission line SED std; nm
-        'line_value': 656.28, # emission line SED std; nm
-        'line_unit': Unit('nm'),
-        'sed_start': 650,
-        'sed_end': 660,
-        'sed_resolution': 0.025,
-        'sed_unit': Unit('nm'),
-        'cov_sigma': 0.5, # pixel counts; dummy value
-        'bandpass_throughput': '.2',
-        'bandpass_unit': 'nm',
-        'bandpass_zp': 30,
-        'priors': {
-            'g1': priors.GaussPrior(0., 0.1),#, clip_sigmas=2),
-            'g2': priors.GaussPrior(0., 0.1),#, clip_sigmas=2),
-            'theta_int': priors.UniformPrior(0., np.pi),
-            # 'theta_int': priors.UniformPrior(np.pi/3, np.pi),
-            'sini': priors.UniformPrior(0., 1.),
-            # 'sini': priors.GaussPrior()
-            'v0': priors.UniformPrior(0, 20),
-            # 'vcirc': priors.GaussPrior(200, 10),#, clip_sigmas=2),
-            'vcirc': priors.UniformPrior(190, 210),
-            'rscale': priors.UniformPrior(0, 10),
-        },
-        'intensity': {
-            # For this test, use truth info
-            # 'type': 'inclined_exp',
-            # 'flux': 1e5, # counts
-            # 'hlr': 5, # pixels
-            'type': 'basis',
-            'basis_type': 'shapelets',
-            'basis_kwargs': {
-                'Nmax': 10,
-                }
-        },
-        # 'psf': gs.Gaussian(fwhm=3), # fwhm in pixels
-        'use_numba': False,
-    }
-
-    # li, le, dl = 655.5, 657, 0.1
-    li, le, dl = 655.8, 656.8, 0.1
-    # li, le, dl = 655.9, 656.8, .1
-    lambdas = [(l, l+dl) for l in np.arange(li, le, dl)]
-
-    Nx, Ny = 30, 30
-    Nspec = len(lambdas)
-    shape = (Nx, Ny, Nspec)
-    print('Setting up test datacube and true Halpha image')
-    datacube, sed, vmap, true_im = likelihood.setup_likelihood_test(
-        true_pars, pars, shape, lambdas
-        )
-
-    outfile = os.path.join(outdir, 'true-im.png')
-    print(f'Saving true intensity profile in obs plane to {outfile}')
-    plt.imshow(true_im, origin='lower')
-    plt.colorbar()
-    plt.title('True Halpha profile in obs plane')
-    plt.savefig(outfile, bbox_inches='tight', dpi=300)
-    if show is True:
-        plt.show()
     else:
-        plt.close()
+        outdir = os.path.join(
+            utils.TEST_DIR, 'test-mcmc-run', run_name
+            )
+        utils.make_dir(outdir)
 
-    outfile = os.path.join(outdir, 'vmap.png')
-    print(f'Saving true vamp in obs plane to {outfile}')
-    plt.imshow(vmap, origin='lower')
-    plt.colorbar(label='v')
-    plt.title('True velocity map in obs plane')
-    plt.savefig(outfile, bbox_inches='tight', dpi=300)
-    if show is True:
-        plt.show()
-    else:
-        plt.close()
+        true_pars = {
+            'g1': 0.05,
+            'g2': -0.025,
+            'theta_int': np.pi / 3,
+            'sini': 0.8,
+            'v0': 10.,
+            'vcirc': 200,
+            'rscale': 5,
+        }
 
-    outfile = os.path.join(outdir, 'datacube.fits')
-    print(f'Saving test datacube to {outfile}')
-    datacube.write(outfile)
+        # additional args needed for prior / likelihood evaluation
+        halpha = 656.28 # nm
+        R = 5000.
+        z = 0.3
+        pars = {
+            'Nx': 30, # pixels
+            'Ny': 30, # pixels
+            'true_flux': 1e5, # counts
+            'true_hlr': 5, # pixels
+            'v_unit': Unit('km / s'),
+            'r_unit': Unit('kpc'),
+            'z': z,
+            'spec_resolution': R,
+            # 'line_std': 0.17,
+            'line_std': halpha * (1.+z) / R, # emission line SED std; nm
+            'line_value': 656.28, # emission line SED std; nm
+            'line_unit': Unit('nm'),
+            'sed_start': 650,
+            'sed_end': 660,
+            'sed_resolution': 0.025,
+            'sed_unit': Unit('nm'),
+            'cov_sigma': 4, # pixel counts; dummy value
+            'bandpass_throughput': '.2',
+            'bandpass_unit': 'nm',
+            'bandpass_zp': 30,
+            'priors': {
+                'g1': priors.GaussPrior(0., 0.1),#, clip_sigmas=2),
+                'g2': priors.GaussPrior(0., 0.1),#, clip_sigmas=2),
+                'theta_int': priors.UniformPrior(0., np.pi),
+                # 'theta_int': priors.UniformPrior(np.pi/3, np.pi),
+                'sini': priors.UniformPrior(0., 1.),
+                # 'sini': priors.GaussPrior()
+                'v0': priors.UniformPrior(0, 20),
+                'vcirc': priors.GaussPrior(200, 10, clip_sigmas=2),
+                # 'vcirc': priors.UniformPrior(190, 210),
+                'rscale': priors.UniformPrior(0, 10),
+            },
+            'intensity': {
+                # For this test, use truth info
+                'type': 'inclined_exp',
+                'flux': 1e5, # counts
+                'hlr': 5, # pixels
+                # 'type': 'basis',
+                # 'basis_type': 'shapelets',
+                # 'basis_kwargs': {
+                #     'Nmax': 10,
+                #     }
+            },
+            # 'psf': gs.Gaussian(fwhm=3), # fwhm in pixels
+            'use_numba': False,
+        }
 
-    outfile = os.path.join(outdir, 'datacube-slices.png')
-    print(f'Saving example datacube slice images to {outfile}')
-    # if Nspec < 10:
-    sqrt = int(np.ceil(np.sqrt(Nspec)))
-    slice_indices = range(Nspec)
+        # li, le, dl = 655.5, 657, 0.1
+        li, le, dl = 655.8, 656.8, 0.1
+        # li, le, dl = 655.9, 656.8, .1
+        lambdas = [(l, l+dl) for l in np.arange(li, le, dl)]
 
-    # else:
-    #     sqrt = 3
-    #     slice_indices = np.sort(
-    #         np.random.choice(
-    #             range(Nspec),
-    #             size=sqrt**2,
-    #             replace=False
-    #             )
-    #         )
+        Nx, Ny = 30, 30
+        Nspec = len(lambdas)
+        shape = (Nx, Ny, Nspec)
+        print('Setting up test datacube and true Halpha image')
+        datacube, sed, vmap, true_im = likelihood.setup_likelihood_test(
+            true_pars, pars, shape, lambdas
+            )
 
-    k = 1
-    for i in slice_indices:
-        plt.subplot(sqrt, sqrt, k)
-        plt.imshow(datacube.slices[i]._data, origin='lower')
+        outfile = os.path.join(outdir, 'true-im.png')
+        print(f'Saving true intensity profile in obs plane to {outfile}')
+        plt.imshow(true_im, origin='lower')
         plt.colorbar()
-        l, r = lambdas[i]
-        plt.title(f'lambda=({l:.1f}, {r:.1f})')
-        k += 1
-    plt.gcf().set_size_inches(12,12)
-    plt.tight_layout()
-    plt.savefig(outfile, bbox_inches='tight', dpi=300)
-    if show is True:
-        plt.show()
-    else:
-        plt.close()
+        plt.title('True Halpha profile in obs plane')
+        plt.savefig(outfile, bbox_inches='tight', dpi=300)
+        if show is True:
+            plt.show()
+        else:
+            plt.close()
 
-    pars['sed'] = sed
+        outfile = os.path.join(outdir, 'vmap.png')
+        print(f'Saving true vamp in obs plane to {outfile}')
+        plt.imshow(vmap, origin='lower')
+        plt.colorbar(label='v')
+        plt.title('True velocity map in obs plane')
+        plt.savefig(outfile, bbox_inches='tight', dpi=300)
+        if show is True:
+            plt.show()
+        else:
+            plt.close()
+
+        outfile = os.path.join(outdir, 'datacube.fits')
+        print(f'Saving test datacube to {outfile}')
+        datacube.write(outfile)
+
+        outfile = os.path.join(outdir, 'datacube-slices.png')
+        print(f'Saving example datacube slice images to {outfile}')
+        # if Nspec < 10:
+        sqrt = int(np.ceil(np.sqrt(Nspec)))
+        slice_indices = range(Nspec)
+
+        # else:
+        #     sqrt = 3
+        #     slice_indices = np.sort(
+        #         np.random.choice(
+        #             range(Nspec),
+        #             size=sqrt**2,
+        #             replace=False
+        #             )
+        #         )
+
+        k = 1
+        for i in slice_indices:
+            plt.subplot(sqrt, sqrt, k)
+            plt.imshow(datacube.slices[i]._data, origin='lower')
+            plt.colorbar()
+            l, r = lambdas[i]
+            plt.title(f'lambda=({l:.1f}, {r:.1f})')
+            k += 1
+        plt.gcf().set_size_inches(12,12)
+        plt.tight_layout()
+        plt.savefig(outfile, bbox_inches='tight', dpi=300)
+        if show is True:
+            plt.show()
+        else:
+            plt.close()
+
+        pars['sed'] = sed
 
     if sampler == 'zeus':
         print('Setting up KLensZeusRunner')
@@ -204,77 +223,85 @@ def main(args):
         print(f' |g1+ig2| = {val}')
         raise e
 
-    runner.burn_in = nsteps // 2
+    if pool.is_master():
+        runner.burn_in = nsteps // 2
 
-    if (ncores > 1) and (sampler == 'zeus'):
-        # The sampler isn't pickleable for some reason in this scenario
-        pass
-    else:
-        outfile = os.path.join(outdir, 'test-mcmc-sampler.pkl')
-        print(f'Pickling sampler to {outfile}')
+        if (ncores > 1) and (sampler == 'zeus'):
+            # The sampler isn't pickleable for some reason in this scenario
+            pass
+        else:
+            outfile = os.path.join(outdir, 'test-mcmc-sampler.pkl')
+            print(f'Pickling sampler to {outfile}')
+            with open(outfile, 'wb') as f:
+                pickle.dump(runner.sampler, f)
+
+            outfile = os.path.join(outdir, 'test-mcmc-runner.pkl')
+            print(f'Pickling runner to {outfile}')
+            with open(outfile, 'wb') as f:
+                pickle.dump(runner, f)
+
+        truth = np.zeros(len(PARS_ORDER))
+        for name, indx in PARS_ORDER.items():
+            truth[indx] = true_pars[name]
+        outfile = os.path.join(outdir, 'test-mcmc-truth.pkl')
+        print(f'Pickling truth to {outfile}')
         with open(outfile, 'wb') as f:
-            pickle.dump(runner.sampler, f)
+            pickle.dump(truth, f)
 
-        outfile = os.path.join(outdir, 'test-mcmc-runner.pkl')
-        print(f'Pickling runner to {outfile}')
-        with open(outfile, 'wb') as f:
-            pickle.dump(runner, f)
+        outfile = os.path.join(outdir, 'chains.png')
+        print(f'Saving chain plots to {outfile}')
+        reference = likelihood.pars2theta(true_pars)
+        runner.plot_chains(
+            outfile=outfile, reference=reference, show=show
+            )
 
-    truth = np.zeros(len(PARS_ORDER))
-    for name, indx in PARS_ORDER.items():
-        truth[indx] = true_pars[name]
-    outfile = os.path.join(outdir, 'test-mcmc-truth.pkl')
-    print(f'Pickling truth to {outfile}')
-    with open(outfile, 'wb') as f:
-        pickle.dump(truth, f)
+        outfile = os.path.join(outdir, 'corner-truth.png')
+        print(f'Saving corner plot to {outfile}')
+        title = 'Reference lines are param truth values'
+        runner.plot_corner(
+            outfile=outfile, reference=truth, title=title, show=show
+            )
 
-    outfile = os.path.join(outdir, 'chains.png')
-    print(f'Saving chain plots to {outfile}')
-    reference = likelihood.pars2theta(true_pars)
-    runner.plot_chains(
-        outfile=outfile, reference=reference, show=show
-        )
+        runner.compute_MAP()
+        map_medians = runner.MAP_medians
+        print('(median) MAP values:')
+        for name, indx in PARS_ORDER.items():
+            m = map_medians[indx]
+            print(f'{name}: {m:.4f}')
 
-    outfile = os.path.join(outdir, 'corner-truth.png')
-    print(f'Saving corner plot to {outfile}')
-    title = 'Reference lines are param truth values'
-    runner.plot_corner(
-        outfile=outfile, reference=truth, title=title, show=show
-        )
+        outfile = os.path.join(outdir, 'compare-data-to-map.png')
+        print(f'Plotting MAP comparison to data in {outfile}')
+        runner.compare_MAP_to_data(outfile=outfile, show=show)
 
-    runner.compute_MAP()
-    map_medians = runner.MAP_medians
-    print('(median) MAP values:')
-    for name, indx in PARS_ORDER.items():
-        m = map_medians[indx]
-        print(f'{name}: {m:.4f}')
+        outfile = os.path.join(outdir, 'compare-vmap-to-map.png')
+        print(f'Plotting MAP comparison to velocity map in {outfile}')
+        vmap_pars = true_pars
+        vmap_pars['r_unit'] = pars['r_unit']
+        vmap_pars['v_unit'] = pars['v_unit']
+        vmap_true = VelocityMap('default', vmap_pars)
+        runner.compare_MAP_to_truth(vmap_true, outfile=outfile, show=show)
 
-    outfile = os.path.join(outdir, 'compare-data-to-map.png')
-    print(f'Plotting MAP comparison to data in {outfile}')
-    runner.compare_MAP_to_data(outfile=outfile, show=show)
-
-    outfile = os.path.join(outdir, 'compare-vmap-to-map.png')
-    print(f'Plotting MAP comparison to velocity map in {outfile}')
-    vmap_pars = true_pars
-    vmap_pars['r_unit'] = pars['r_unit']
-    vmap_pars['v_unit'] = pars['v_unit']
-    vmap_true = VelocityMap('default', vmap_pars)
-    runner.compare_MAP_to_truth(vmap_true, outfile=outfile, show=show)
-
-    outfile = os.path.join(outdir, 'corner-map.png')
-    print(f'Saving corner plot compare to MAP in {outfile}')
-    title = 'Reference lines are param MAP values'
-    runner.plot_corner(
-        outfile=outfile, reference=runner.MAP_medians, title=title, show=show
-        )
+        outfile = os.path.join(outdir, 'corner-map.png')
+        print(f'Saving corner plot compare to MAP in {outfile}')
+        title = 'Reference lines are param MAP values'
+        runner.plot_corner(
+            outfile=outfile, reference=runner.MAP_medians, title=title, show=show
+            )
 
     return 0
 
 if __name__ == '__main__':
+    pudb.set_trace()
+    args = parser.parse_args()
+
+    pool = schwimmbad.choose_pool(
+        mpi=args.mpi, processes=args.ncores
+        )
+
     args = parser.parse_args()
 
     print('Starting tests')
-    rc = main(args)
+    rc = main(args, pool)
 
     if rc == 0:
         print('All tests ran succesfully')

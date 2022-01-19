7446e67a17dad2a616438520d3d6c669d37abcd0
Spencer Everett <spencerweverett@gmail.com>
2022-01-19 15:16:52 -0800

    Added new `Pars`, `SampledPars`, and `MetaPars` classes

--- kl_tools/parameters.py
+++ kl_tools/parameters.py
@@ -1,3 +1,5 @@
+import utils
+
 import pudb
 
 '''
@@ -15,6 +17,139 @@ TODO: To make this future-proof, what we should do is make a function that
       *returns* PARS_ORDER given a velocity model name. This way it is accessible
       throughout, but allows things to be a bit more flexible
 '''
+class Pars(object):
+    '''
+    Holds all of the parameters for a needed MCMC run, both
+    sampled and meta parameters
+    '''
+
+    def __init__(self, sampled_pars, meta_pars):
+        '''
+        sampled_pars: list of str's
+            A list of parameter names to be sampled in the MCMC.
+            Their order will be used to define pars2theta
+        meta_pars: dict
+            A dictionary of meta parameters and their values for
+            a particular experiment and MCMC run
+        '''
+
+        args = {
+            'sampled_pars': (sampled_pars, list),
+            'meta_pars': (meta_pars, dict)
+            }
+        utils.check_types(args)
+
+        for name in sampled_pars:
+            utils.check_type(name, 'sampled_par_val', str)
+
+        pars_order = dict(zip(sampled_pars, range(len(sampled_pars))))
+        self.sampled = SampledPars(pars_order)
+        self.meta = MetaPars(meta_pars)
+
+        return
+
+class SampledPars(object):
+    '''
+    Sets the structure for arbitrary sampled parameters, which
+    are stored internally as a fixed list in the samplers
+    '''
+
+    def __init__(self, pars_order):
+        '''
+        pars_order: dict
+            A dictionary that defines the par_name: sampler_index
+            relationship in the used sampler
+
+            For example:
+            pars_order = {
+                'g1': 0,
+                'g2': 1,
+                ...
+            }
+        '''
+
+        utils.check_type(pars_order, 'pars_order', dict)
+
+        for key, val in pars_order.items():
+            if not isinstance(val, int):
+                raise TypeError('pars_order must have int values!')
+
+        self.pars_order = pars_order
+
+        return
+
+    def theta2pars(self, theta):
+        '''
+        uses pars_order to convert list of sampled params to dict
+        '''
+
+        assert len(theta) == len(self.pars_order)
+
+        pars = {}
+        for key, indx in self.pars_order.items():
+            pars[key] = theta[indx]
+
+        return pars
+
+    def pars2theta(self, pars):
+        '''
+        convert dict of parameters to theta list
+        '''
+
+        assert len(pars) == len(self.pars_order)
+
+        # initialize w/ junk that will fail if not set correctly
+        theta = len(PARS_ORDER) * ['']
+
+        for name, indx in PARS_ORDER.items():
+            theta[indx] = pars[name]
+
+        return theta
+
+class MetaPars(dict):
+    '''
+    Class that defines structure for the general parameters
+    used in MCMC sampling for a given experiment, modeling
+    choices, etc.
+    '''
+
+    _req_fields = ['intensity', 'psf', 'sed']
+
+    def __init__(self, pars):
+        '''
+        pars: dict
+            Dictionary of meta pars
+        '''
+
+        utils.check_type(pars, 'pars', dict)
+
+        self._check_pars(pars)
+        self.pars = pars
+
+        return
+
+    @classmethod
+    def _check_pars(cls, pars):
+        '''
+        Make sure that the general parameter list
+        contains a few required entries
+        '''
+
+        for key in cls._req_fields:
+            if key not in pars:
+                return KeyError(f'{key} is a required field ' +\
+                                'in the parameter list!')
+
+        return
+
+    def __getitem__(self, key):
+        return self.pars[key]
+
+    def __setitem__(self, key, val):
+        return self.pars[key] = val
+
+    def __delitem__(self, key):
+        return del self.pars[key]
 
 # order of sampled mcmc parameters
 # NOTE: This won't be accessible if we use numba

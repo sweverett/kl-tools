8b897ab336d84116af6aff6da04be21752eefe2c
emhuff <emhuff@gmail.com>
2022-07-05 10:42:55 -0700

    muse datacube updates

--- kl_tools/muse_datacube.py
+++ kl_tools/muse_datacube.py
@@ -47,12 +47,17 @@ class MuseDataCube(cube.DataCube):
         hdr3d = fitsio.read_header(cubePath)
         self._cubewcs = wcs.WCS(hdr3d)
         self._spec1d = fitsio.read(specPath)
-        self._catalogEntry = fitsio.read(catalogPath)
-        self._set_parameters()
+        catalog = fitsio.read(catalogPath)
+        objid = re.search('(\d+)_*',cubePath.name).groups()[0]
+        fullCatalog = fitsio.read(catalogPath)
+        self._catalogEntry = fullCatalog[fullCatalog['UNIQUE_ID'] == objid]
         
         self.Nx = spec3d.shape[1]
         self.Ny = spec3d.shape[2]
         self.Nspec = spec3d.shape[0]
+        scales = self._cubewcs.proj_plane_pixel_scales()
+        self.pixel_scale = np.sqrt(scales[0].to(u.arcsec)*scales[1].to(u.arcsec)).value
+        self._set_parameters()
         # Looks like the likelihood code expects the datacube to have a slightly different axis order.
         datacube = np.moveaxis(spec3d,0,-1)
         # Make the list of bandpasses.
@@ -60,19 +65,28 @@ class MuseDataCube(cube.DataCube):
         dlam = np.append(dlam,dlam[-1])
         # Now dlam should be an array of spaxel widths. 
         bandpasses = [gs.Bandpass(1.0, 'A', blue_limit = il-dl, red_limit = il+dl, zeropoint = 22.5) for il,dl in zip(self._spec1d['WAVE_VAC'],dlam)]
-        super().__init__(data=datacube,shape=datacube.shape, bandpasses=bandpasses,pix_scale = pixel_scale)
 
-        def _set_parameters(self):
-            '''
-            return a parameter dictionary populated with (some) of the fields that will be needed in the likelihood parameters dictionary, based on the data.
-            '''
-            scales = self._cubewcs.proj_plane_pixel_scales()
-            self._pars = {}
-            self.pars['pix_scale'] = np.sqrt(scales[0].to(u.arcsec)*scales[1].to(u.arcsec)).value # Use a geometric average to keep the right pixel area, in case the pixels aren't square.
-            self.pars['Nx'] = self.Nx
-            self.pars['Ny'] = self.Ny
-            self.pars['bandpass_throughput'] = 0.2 # A guess, based on throughput here: https://www.eso.org/sci/facilities/paranal/instruments/muse/inst.html
+        super().__init__(data=datacube,shape=datacube.shape, bandpasses=bandpasses,pix_scale = self.pars['pix_scale'] )
 
+        # Once everything is set up for MUSE, populate the parameters dictionary that the runner will need.
+        
+
+        
+    def _set_parameters(self):
+        '''
+        return a parameter dictionary populated with (some) of the fields that will be needed in the likelihood parameters dictionary, based on the data.
+        '''
+        
+        self.pars = {}
+        self.pars['pix_scale'] = self.pixel_scale # Use a geometric average to keep the right pixel area, in case the pixels aren't square.
+        self.pars['Nx'] = self.Nx
+        self.pars['Ny'] = self.Ny
+        self.pars['bandpass_throughput'] = 0.2 # A guess, based on throughput here: https://www.eso.org/sci/facilities/paranal/instruments/muse/inst.html
+        self.pars['z'] = self._catalogEntry['Z']
+
+        # These MUSE observations have a limited spectral range, between 
+        
+        self.line_value = 
 
 if __name__ == '__main__':
     testpath = pathlib.Path("../tests/testdata")
@@ -81,5 +95,5 @@ if __name__ == '__main__':
     catalogPath = testpath / pathlib.Path("MW_1-24_main_table.fits")
     
     # Try initializing a datacube object with these paths.
-    thisCube = MuseDataCube(filename3d = spec3dPath, filename1d = spec1dPath, catalogFile=catalogPathNe)
+    thisCube = MuseDataCube(filename3d = spec3dPath, filename1d = spec1dPath, catalogFile=catalogPath)
     ipdb.set_trace()

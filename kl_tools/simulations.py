bed7da8c4141182133b198ef082377e0a0607b1d
emhuff <emhuff@gmail.com>
2022-08-09 15:38:09 -0700

    added remote query and first elements of code to calculate spectra from TNG sim

--- kl_tools/simulations.py
+++ kl_tools/simulations.py
@@ -1,10 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
+import astropy.units as u
+import astropy.constants as const
+from astropy import cosmology
+cosmo = cosmology.Planck18_arXiv_v2
+
 
 verbose = False
 baseURL = 'http://www.tng-project.org/api/'
 headers = {"api-key":"b703779c1f099efed6f47b91607b1bb1"}
+
+rbase = get(baseUrl)
+use_sim = 'TNG50-1'
+names = [sim['name'] for sim in rbase['simulations']]
+i = names.index(use_sim)
+sim = get( rbase['simulations'][i]['url'] )
+snaps = get( sim['snapshots'] )
+snap = get( snaps[-1]['url'] )
+
+subs = get( snap['subhalos'], {'limit':50000, 'order_by':'-mass_stars'} )
 
 
 class Simulation():
@@ -12,10 +27,13 @@ class Simulation():
         pass
 
 
-class TNGsimulation():
+class TNGsimulation(Simulation):
     def __init__(self):
         pass
 
+    def set_redshift(self,z=0.1):
+        self.z = z
+    
     @classmethod
     def from_datacube(cls, datacube, **kwargs):
 
@@ -25,60 +43,90 @@ class TNGsimulation():
         '''
         pars = {}
         sim = Simulation(pars)
+        self._datacube = datacube
+        self._lambda1d = np.array( [(bp.red_limit + bp.blue_limit)/2. for bp in datacube.bandpasses] )
         return sim
 
-    def _getFITS(self,params=None):
-
-        '''
-        We expect the output to be a fits file.
-
-        '''
-        r = requests.get(path, params=params)
-
-        # raise exception if response code is not HTTP SUCCESS (200)
-        r.raise_for_status()
+    def _calculate_gas_temperature(self,h5data):
+        u           = h5data['PartType0']['InternalEnergy'][:]    #  the Internal Energy
+        Xe          = h5data['PartType0']['ElectronAbundance'][:]  # xe (=ne/nH)  the electron abundance
+        XH          = 0.76             # the hydrogen mass fraction
+        gamma        = 5.0/3.0          # the adiabatic index
+        KB          = 1.3807e-16       # the Boltzmann constant in CGS units  [cm^2 g s^-2 K^-1]
+        mp          = 1.6726e-24       # the proton mass  [g]
+        little_h    = 0.704                 # NOTE: 0.6775 for all TNG simulations
+        mu          = (4*mp)/(1+3*XH+4*XH*Xe)
         
-        # Parse this into an astropy FITS.. thing.
-        hdulist = fits.open(file_stream, memmap=True, do_not_scale_image_data=True)
-        # From here:
-        #  https://github.com/astropy/astropy/issues/7980
+        # Estimate temperature  
+        temperature = (gamma-1)* (u/KB)* mu* 1e10
+        return temperature
 
+
+    def _gas_line_flux(self, h5data):
         
-    def _getIllustrisData(self,haloid, snap, partType = 'gas', field = 'sb_H--1-1215.67A',fov = 2.):
-        #print(f"field:{field}")
-        base_visurl = snap['url']+f'subhalos/{haloid}/vis.hdf5'
-        # Each visualization has an associated a particle type and display field.
-        imgSize = fov
-        imgSizeUnit = 'arcmin'
-        url = base_visurl+f'?partType={partType}&partField={field}&size={imgSize}&sizeType={imgSizeUnit}'
-        # Requests writes this to a file and returns the filename. So 
-        #print(f"query url: {url}")
+
+        T = self._calculate_gas_temperature(h5data)
+        h = cosmo.h # hubble parameter
+        alpha = 2.6e-13 * (T/1e4)**(-0.7)  * u.cm**3 / u.s 
+        Xe = h5data['PartType0']['ElectronAbundance'][:]
+        XH = 0.76
+        nH =  (h5data['PartType0']['Density']*1e10 * u.M_sun / u.kpc**3 * h**2 / const.m_e).to(1/u.cm**3)
+        ne = Xe * nH
+        V = (h5data['PartType0']['Mass']* 1e10 * u.M_sun/h) / ( h5data['PartType0']['Density'] * 1e10 * u.M_sun/h / (u.kpc / h)***3)
+        # Number of recombinations in this volume element
+        nr = alpha * ne * nH * V
+
+        # What's the flux from this particle at the observer?
+        dL = cosmo.luminosity_distance(z)
+        photon_flux = ( nr / (4 * np.pi * dL**2) ).to(1/u.cm**2/u.s)
+
+        return photon_flux
+    
+    def _star_particle_flux(self, h5data):
+
+        # This should just return continuum proportional to stellar mass.
+        # Maybe an age correction.
+        # Normalize a solar spectrum:
+        norm = 1 * u.Watt / u.m**2 / u.nm / const.M_sun
+        spec_norm = norm * (h5data['PartType4']['Masses']*u.M_sun)
+        return spec_norm
+
+    def _linespec(self,h5data,line_flux, line_center = 6563. * u.Angstrom, resolution = 5000):
+        '''
+        resolution -- spectral resolution at line center; unitless, means Lambda/Delta Lambda
+        '''
+
+        sigma * line_center / resolution
+        # Calculate the velocity offset of each particle.
+        dv = h5data['PartType0']['Velocities'][:,0] * np.sqrt(1/1+self.z) * u.km/u.s
+        # get physical velocities from TNG by multiplying by sqrt(a)
+        # https://www.tng-project.org/data/docs/specifications/#parttype0
+        dlam = line_center *  dv / const.c
+        
+        line_spectra = line_flux/np.sqrt(2*np.pi*sigma**2) * np.exp(-(self._lambda1 - (line_center + dlam))**2/sigma/2.)
+        return line_spectra
+        
+        
+    def _getIllustrisTNGData(self,subhaloid=100, line_center = 6563*u.Angstrom):
+        '''
+        For a chosen haloid and snapshot, get the ingredients necessary to build a simulated datacube.
+        This means stellar continuum (flat across our SED) and a line that traces the gas.
+        '''
+
+        sub = get( subs['results'][subhaloid]['url'] )        
+        url = f"http://www.tng-project.org/api/{use_sim}/snapshots/{sub['snap']}/subhalos/{sub['id']}/cutout.hdf5"
         r = requests.get(url,headers=headers)
-        f = io.BytesIO(r.content)
-        h = h5py.File(f,'r')
-        data = h['grid'][:] # this is where Illustris likes to put the returned data.
-        # Note: pixels with no subhalo information in them will get a 'nan' in flux, but will be 99 in FLAM. Be sure to get that later.
-        # Add units to this, depending on what was requested.
-        if 'sb_H' in field:
-            flux = (10.**data) * u.photon / u.s /u.cm**2 / u.arcsec**2
-        if 'stellarBand' in field:
-            # work out the source flux, in photon/s/cm2/arcsec2
-            dmod = cosmo.distmod(snap['redshift'])
-            mag = data*u.ABmag + dmod
-        if 'nuv' in field:
-            lambda_eff =2304.74*u.AA  # from the SVO filter profile service, GALEX NUV
-            lambda_width = 768.31*u.AA # from the SVO filter profile service, GALEX NUV
-        if 'fuv' in field: 
-            lambda_eff = 1555.87*u.AA    # from the SVO filter profile service, GALEX FUV
-            lambda_width = 265.57 * u.AA # from the SVO filter profile service, GALEX FUV
-        if 'sdss_g' in field:
-            lambda_eff = 4671.78*u.AA
-            lambda_width = 1064.68*u.AA
-        flux = mag.to(u.photon/u.s/u.cm**2/u.AA,u.spectral_density(lambda_eff))*lambda_width
+        f = BytesIO(r.content)
+        h = h5py.File(f,mode='r')
+        Temp = 
+        # Assign an emission-line flux and star particle continuum level to each star and gas particle
         
-    bad = ~np.isfinite(flux)
-    flux[bad] = 0.
-    return flux
+        self._particleData = h
+        self._particleTemp = self._calculate_gas_temperature(h)
+        self._starFlux = self._star_particle_flux(h)
+        self._line_flux = self._gas_line_flux(h)
         
-            
-
+        # Once we have these data, we then need to generate an SED for each star and gas particle!
+        particle_spec = self._linespec(h,self._line_flux, line_center = line_center)
+        
+        

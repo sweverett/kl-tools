7c9280861c18c244e5bb2ae56444566cf30bb148
JiachuanXu <jiachuanxu@email.arizona.edu>
2025-07-09 15:35:22 -0700

    add MPI test for pocoMC

--- scripts/test_poco.py
+++ scripts/test_poco.py
@@ -7,6 +7,22 @@ os.environ['OMP_NUM_THREADS'] = '1'
 from scipy.stats import uniform, norm
 import numpy as np
 import ipdb
+try:
+    import schwimmbad
+    import mpi4py
+    from mpi4py import MPI
+except:
+    print("Can not import MPI, use single process")
+try:
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+except:
+    rank = 0
+    size = 1
+from schwimmbad import MPIPool
+pool = schwimmbad.choose_pool(mpi=True, processes=1)
+
 
 def main():
     import pocomc as pc
@@ -17,14 +33,14 @@ def main():
     prior = pc.Prior(ndim * [uniform(loc=-10.0, scale=20.0)] )
 
     # Define our 10-D Rosenbrock log-likelihood.
-    def log_likelihood(x):
+    def log_likelihood(x, flag=True):
         # print('x = ',x)
         #ipdb.set_trace()
         return -np.sum(10.0*(x[:,::2]**2.0 - x[:,1::2])**2.0 \
                 + (x[:,::2] - 1.0)**2.0, axis=1)
 
     # Number of particles to use
-    nparticles = 1000
+    nparticles = 512
 
     # Initialise sampler
     #sampler = pc.Sampler(nparticles,
@@ -33,25 +49,25 @@ def main():
     #                     log_prior=log_prior,
     #                     vectorize_likelihood=True,
     #                     bounds=(-10.0, 10.0))
-    sampler = pc.Sampler(n_dim=ndim, 
+    with pool:
+        if isinstance(pool, schwimmbad.MPIPool):
+            if not pool.is_master():
+                pool.wait()
+                sys.exit(0)   
+        sampler = pc.Sampler(n_dim=ndim, 
                          n_effective=nparticles,
                          n_active = nparticles//2,
                          prior=prior, 
                          likelihood=log_likelihood,
+                         likelihood_kwargs={'flag': False},
                          vectorize=True,
                          output_dir="../tests/test_poco",
                          output_label="checkpoint",
+                         pool=pool,
                          )
-
-    # Initialise particles' positions using samples from the prior (this is very important, other initialisation will not work).
-    prior_samples = np.random.uniform(low=-10.0, high=10.0, size=(nparticles, ndim))
-
-    # Start sampling
-    #sampler.run(prior_samples)
-    sampler.run(save_every=10)
-
-    # We can add more samples at the end
-    #sampler.add_samples(1000)
+        # Start sampling
+        #sampler.run(prior_samples)
+        sampler.run(n_total=4096, save_every=10)
 
     # Save state
     sampler.save_state("../tests/test_poco/final_state")

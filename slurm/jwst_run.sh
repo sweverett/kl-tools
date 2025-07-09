1f4d4f58e20c264e6d8f82dcb2cb11a6760f15c5
JiachuanXu <jiachuanxu@email.arizona.edu>
2025-07-09 15:36:45 -0700

    misc updates

--- slurm/jwst_run.sh
+++ slurm/jwst_run.sh
@@ -15,7 +15,7 @@
 
 #SBATCH --account=timeifler
 
-#SBATCH --time=48:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jiachuanxu@arizona.edu
 
@@ -32,11 +32,30 @@ conda activate kltools
 #MPIRUN=/opt/ohpc/pub/mpi/openmpi5-gnu13/5.0.5/bin/mpirun
 MPIRUN=/opt/ohpc/pub/mpi/openmpi3-gnu8/3.1.4/bin/mpirun
 
-YAML=../yaml/example_jwst_noshear_freekine.yaml
+### Real data
+#YAML=../yaml/example_jwst_noshear_freekine.yaml
+#YAML=../yaml/example_jwst_shear_TFR_short.yaml
+#YAML=../yaml/example_jwst_noshear_TFR_short.yaml
+YAML=../yaml/example_jwst_shear_TFR_full_D2.yaml
+
+### Mock data
+#YAML=../yaml/mock_jwst_shear_TFR_smooth8.yaml
+#YAML=../yaml/mock_jwst_shear_TFR.yaml
+#YAML=../yaml/mock_jwst_shear_TFR_full.yaml
+
+### pocoMC config
+#N_EFFECTIVE=512
+#N_TOTAL=4096
+N_EFFECTIVE=2048
+N_TOTAL=16384
 
 MPI_PML="--mca pml ob1"
 MPI_BTL="--mca btl tcp,self"
-${MPIRUN} -n ${SLURM_NTASKS} ${MPI_BTL} python test_jwst_mcmc_run.py ${YAML} --mpi
 
+### emcee
+#### ${MPIRUN} -n ${SLURM_NTASKS} ${MPI_BTL} python test_jwst_mcmc_run.py ${YAML} --mpi
+
+### pocoMC
+${MPIRUN} -n ${SLURM_NTASKS} ${MPI_BTL} python test_jwst_mcmc_run.py ${YAML} --mpi -sampler=pocomc -nparticles=${N_EFFECTIVE} -n_total=${N_TOTAL} --mpi
 
 
